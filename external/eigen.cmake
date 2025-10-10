53e140a1e8119a7dc0c553c296cc5d63dd1b63b2
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2025-10-10 14:55:44 +0900

    Eigen3のFetchContent宣言を修正し、ライブラリ名を統一

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -31,13 +31,13 @@ endif()
 
 #### Eigen ####
 FetchContent_Declare(
-    eigen3
+    eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.4.1
     GIT_SHALLOW     TRUE
     )
 
-FetchContent_MakeAvailable(eigen3)
+FetchContent_MakeAvailable(eigen)
 
 add_library(openjij-eigen_lib INTERFACE)
 target_include_directories(openjij-eigen_lib INTERFACE ${eigen_SOURCE_DIR})
